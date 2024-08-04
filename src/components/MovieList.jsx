9f17b05afd048b31d814e703970f85822624a703
Bibo Boom <77632683+BiboBoom10@users.noreply.github.com>
2024-08-05 00:32:44 +0300

    added movie card

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,61 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Dimensions, Image, ScrollView, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+import starWarsImage from '../../assets/starwars.jpg'; 
+
+const { height, width } = Dimensions.get('window');
 
 function MovieList({title, data}) {
+
+  let movieName = 'Ant Man';
+
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>MovieList</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{color: 'white', fontSize: 20}}>Upcoming</Text>
+        <TouchableOpacity>
+          <Text style={{color: '#eab308'}}>See All</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{paddingHorizontal: 16}}>
+        {data.map((item, index) => {
+          return (
+            <TouchableWithoutFeedback key={index} onPress={() => navigation.navigate('Movie', item)}>
+                <View style={styles.movieCard}>
+                  <Image source={starWarsImage} style={styles.imageCard} />
+                  <Text>{movieName}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+          )
+        })}
+      </ScrollView>
     </View>
   )
 }
 
-export default MovieList
+export default MovieList;
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 8,
+    marginHorizontal: 4
+  },
+  header: {
+    marginHorizontal: 16,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  movieCard: {
+    marginVertical: 8,
+    marginRight: 4
+  },
+  imageCard: {
+    borderRadius: 16,
+    width: width * 0.3,
+    height: height * 0.2
+  }
+});
